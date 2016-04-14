77b675ac375ca1974bfd331c5f02a95636435863
Marije Rooze <marije.rooze@geodan.nl>
2016-04-14 14:09:45 +0200

    splitting up functions, fixed bug with reselecting item after deselecting

--- js/app.js
+++ js/app.js
@@ -1,40 +1,13 @@
-var urlData = 'data/data.json';
-
 function getData(url, callback) {
 	d3.json(url, function(error, data) {
 		callback(data.features);
 	})
 }
 
-var photoGallery = {
-	imgBaseSrc: 'http://saturnus.geodan.nl/geodanmiddag/uploads/',
-	data: [],
-	selectedAuthor: '',
+var authorFilter = {
+	render: function() {
+		var data = model.getAuthorList();
 
-	createGallery: function(data) {
-		this.data = data;
-		this.createPhotoGallery(data);
-
-		var authorList = this.createAuthorList(data);
-		this.renderAuthorList(authorList);
-	},
-
-	createAuthorList: function(data) {
-		var nested_data = d3.nest()
-			.key(function(d) { return d.properties.username; })
-			.rollup(function(ids) {
-			    return ids.length; 
-			})
-			.entries(data);
-
-		nested_data.sort(function(x, y){
-		   return d3.descending(x.values, y.values);
-		})
-
-		return nested_data;
-	},
-
-	renderAuthorList: function(data) {
 		var svg = d3.select("#filter-list")
 			.selectAll('.filter-authors')
 			.data(data)
@@ -53,14 +26,26 @@ var photoGallery = {
 		    });
 
 		    svg.on('click', function(d){
-		    	photoGallery.filterPhotos(d.key);
+		    	controller.filterAuthors(d.key);
 		    })
-	},
+	}
+}
 
-	filterPhotos: function( name ) {
+var timelineFilter = {
+	render: function() {
+
+	}
+}
+
+var photoGallery = {
+	imgBaseSrc: 'http://saturnus.geodan.nl/geodanmiddag/uploads/',
+	selectedAuthor: '',
+
+	filterByAuthor: function( name ) {
 		var isSame = false;
 		if (this.selectedAuthor === name) {
 			isSame = true;
+			name = '';
 		}
 
 		this.selectedAuthor = name;
@@ -76,10 +61,10 @@ var photoGallery = {
 			});
 	},
 
-	createPhotoGallery: function(data) {
-		var self = this;
+	render: function() {
+		var data = model.getData();
 
-		var svg = d3.select("#photo-container")
+		var svg = d3.select('#photo-container')
 					.selectAll('.item')
 					.data(data, function(d){
 						return d.properties.photoid;
@@ -97,20 +82,64 @@ var photoGallery = {
 				return d.properties.username;
 			})
 			.attr('target', '_blank')
-			.append("img")
-		  	.attr("xlink:href", "myimage.png")
-		    .attr("src",function (d) {
+			.append('img')
+		  	.attr('xlink:href', 'myimage.png')
+		    .attr('src',function (d) {
 		    	return photoGallery.imgBaseSrc + d.properties.photoid;
 		    })
-		    .attr("width", 100)
-		    .attr("height", 100);
+		    .attr('width', 100)
+		    .attr('height', 100);
 	}
 };
 
-getData(urlData, function(data) {
-	photoGallery.createGallery(data);
-});
+var model = {
+	urlData: 'data/data.json',
+	data: [],
 
+	init: function(fn) {
+		getData(this.urlData, function(data) {
+			model.data = data;
+			fn(data);
+		});
+	},
+
+	getAuthorList: function() {
+		var data = model.getData();
+
+		var nested_data = d3.nest()
+			.key(function(d) { return d.properties.username; })
+			.rollup(function(ids) {
+			    return ids.length; 
+			})
+			.entries(data);
+
+		nested_data.sort(function(x, y){
+		   return d3.descending(x.values, y.values);
+		})
+
+		return nested_data;
+	},
+
+	getData: function() {
+		return model.data;
+	}
+}
+
+var controller = {
+	init: function() {
+		model.init(function(data) {
+			photoGallery.render();
+
+			authorFilter.render();
+		})
+	},
+
+	filterAuthors: function(name) {
+		photoGallery.filterByAuthor(name);
+	}
+}
+
+controller.init();
 
 
 
